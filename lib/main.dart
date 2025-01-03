ba718a4340de7938dec5b26634893a66aaefe189
FairuzFaiz <ffawwaiz@gmail.com>
2025-01-03 09:30:17 +0700

    building device preview

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,25 @@ import 'package:memory_stitch/Pages/home_screen.dart';
 import 'package:memory_stitch/Pages/login_page.dart';
 import 'package:memory_stitch/Pages/login_email_page.dart';
 import 'package:memory_stitch/Pages/register_page.dart';
-
+import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter/foundation.dart';
+import 'package:provider/provider.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MemoryStitchApp());
+
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode, // Aktifkan hanya di mode debug
+      builder: (context) => const MemoryStitchApp(),
+    ),
+  );
 }
 
 class MemoryStitchApp extends StatelessWidget {
@@ -22,6 +31,10 @@ class MemoryStitchApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      locale:
+          DevicePreview.locale(context), // Ambil pengaturan dari DevicePreview
+      builder: DevicePreview.appBuilder, // Gunakan builder dari DevicePreview
       title: 'Memory Stitch',
       theme: ThemeData(
         primarySwatch: Colors.brown,
